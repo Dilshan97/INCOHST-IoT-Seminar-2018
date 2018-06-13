75ef1568b3537373411879e638acf562d3bcadd0
Dialog IoT <33410831+dialogiot@users.noreply.github.com>
2018-06-13 17:35:15 +0530

    LED example code

---
+++ LED/LED.ino
@@ -0,0 +1,10 @@
+//Device Details
+String DEVICE_BRAND              = "generic_brand_488"; //Replace with your brand name
+String DEVICE_TYPE               = "generic_device";    //Replace with your type name
+String DEVICE_VERSION            = "v1";                //Replace with your version name
+String MAC_ADDRESS               = "3405613838186307";  //Replace with your MAC address Example- 1902100379354041
+
+//Your Wifi Credentials
+const char* ssid                 = "myusername";  //Replace with your WiFi username
+const char* password             = "pass123#";      //Replace with your WiFi password
+
---
+++ LED/your_actions_events.ino
@@ -0,0 +1,25 @@
+void do_actions(const char* message) {
+  //Create this function according to your actions. you will receive a message something like this
+Serial.println(message);
+   
+StaticJsonBuffer<200> jsonBuffer;
+
+JsonObject& root = jsonBuffer.parseObject(message);
+
+String action = root["action"];
+
+JsonObject& param = root["param"]; 
+String param_mac = param["mac"]; 
+
+  if(param_mac.equals(MAC_ADDRESS)){
+    if(action == "on"){
+    digitalWrite(D1,HIGH);
+    Serial.println("on");
+} 
+    if(action == "off"){
+    digitalWrite(D1,LOW);
+    Serial.println("off");
+    }      
+  }
+}
+//====================================================================================================================================================================
